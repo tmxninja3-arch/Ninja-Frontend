74847963e14019fa631008d7cceb50017a64c5a7
tmxninja3-arch <pram33045@gmail.com>
2025-11-27 15:02:06 +0530

    small change

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL ;
+
+
 
 // Create axios instance
 const api = axios.create({
